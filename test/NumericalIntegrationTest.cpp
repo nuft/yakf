c2902c114e65805c7708d7f575337906fb38de2b
Michael Spieler <michael.spieler@epfl.ch>
2018-03-03 18:29:14 +0100

    First unit tests using gtest

---
+++ test/NumericalIntegrationTest.cpp
@@ -0,0 +1,50 @@
+#include "gtest/gtest.h"
+#include "NumericalIntegration.h"
+#include "Eigen/Dense"
+
+using namespace KalmanFilter;
+
+struct ZeroDynamics
+{
+    void operator()(const double &x, double &dx) const {dx = 0.0;}
+};
+
+TEST(EulerTest, TestZeroDynamics) {
+    NumericalIntegration<ZeroDynamics, double, double, 1e-3, Euler> ni;
+    double res;
+    const double x0 = 42;
+    ni.integrate(1, x0, res);
+    ASSERT_EQ(x0, res);
+}
+
+struct ConstDynamics
+{
+    void operator()(const double &x, double &dx) const {dx = 21.0;}
+};
+
+TEST(EulerTest, TestConstDynamics) {
+    NumericalIntegration<ConstDynamics, double, double, 1e-3, Euler> ni;
+    double res;
+    const double x0 = 0;
+    ni.integrate(2, x0, res);
+    EXPECT_NEAR(42.0, res, 0.1);
+}
+
+using Vec2d = Eigen::Vector2d;
+struct FreeFall
+{
+    void operator()(const Vec2d &x, Vec2d &dx) const {
+        dx[0] = x[1];
+        dx[1] = -9.81;
+    }
+};
+
+TEST(EulerTest, TestMultivariateIntegration) {
+    NumericalIntegration<FreeFall, Vec2d, double, 1e-3, Euler> ni;
+    Vec2d res;
+    const Vec2d x0(0, 0);
+    double delta_t = 3.0;
+    ni.integrate(3, x0, res);
+    EXPECT_NEAR(-0.5*9.81*delta_t*delta_t, res[0], 1.0);
+    EXPECT_NEAR(-9.81*delta_t, res[1], 1.0);
+}
