d9a01f4a34d19524675aea148c15d3913a5ec0e5
Michael Spieler <michael.spieler@epfl.ch>
2018-03-04 09:19:21 +0100

    Class documentation, parameter naming

--- NumericalIntegration.h
+++ NumericalIntegration.h
@@ -8,6 +8,12 @@ enum IntegrationMode {
     RungeKutta
 };
 
+/** Numerical integrator class template
+ * \param Functor function to be integrated
+ * \param ValueType input and output type of the functor
+ * \param Scalar scalar type, double or float
+ * \param IntegrationMode choose Euler or RungeKutta method
+ */
 template<typename Functor,
          typename ValueType,
          typename Scalar,
@@ -18,9 +24,10 @@ private:
     Functor f;
 
     // forward Euler method
-    inline void ForwardEuler(Scalar time_span, const ValueType &x0, ValueType &x)
+    inline void ForwardEuler(Scalar time_span, const ValueType &x0, ValueType &xout)
     {
         ValueType dx;
+        ValueType &x = xout;
         for (x = x0; time_span > 0; time_span -= h) {
             f(x, dx);
             x += h * dx;
@@ -28,9 +35,10 @@ private:
     }
 
     // 4th order Runge-Kutta (RK4) method
-    inline void RK4(Scalar time_span, const ValueType &x0, ValueType &x)
+    inline void RK4(Scalar time_span, const ValueType &x0, ValueType &xout)
     {
         ValueType k1, k2, k3, k4;
+        ValueType &x = xout;
         for (x = x0; time_span > 0; time_span -= h) {
             f(x, k1);
             f(x + h / 2 * k1, k2);
@@ -41,17 +49,27 @@ private:
     }
 
 public:
+    /** Numerical integrator constructor
+     * \param time_step integration step size
+     * \note In case of Runke-Kutta method the intermediate step size is half of
+             time_step size.
+     */
     NumericalIntegration(Scalar time_step) : h(time_step) {}
 
-    void integrate(Scalar time_span, const ValueType &x0, ValueType &x)
+    /** Integrate given functor
+     * \param time_span integration time span
+     * \param x0 initial value
+     * \param xout integration result
+     */
+    void integrate(Scalar time_span, const ValueType &x0, ValueType &xout)
     {
         switch (mode) {
             case Euler:
-                ForwardEuler(time_span, x0, x);
+                ForwardEuler(time_span, x0, xout);
                 break;
 
             case RungeKutta:
-                RK4(time_span, x0, x);
+                RK4(time_span, x0, xout);
                 break;
 
             default:
