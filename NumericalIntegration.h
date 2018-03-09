1b76c7af3e5142a8bd543d3a721a2c7dd5e82a64
Michael Spieler <michael.spieler@epfl.ch>
2018-03-09 16:41:30 +0100

    Add step size argument

--- NumericalIntegration.h
+++ NumericalIntegration.h
@@ -21,56 +21,49 @@ public:
     using ValueType = typename Functor::ValueType;
     using Scalar = typename Functor::Scalar;
 private:
-    const Scalar h;
     Functor f;
 
     // Explicit Euler method
-    inline ValueType Euler(Scalar time_span, const ValueType &x0)
+    inline ValueType Euler(Scalar time_span, Scalar step, const ValueType &x0)
     {
         ValueType x, dx;
-        for (x = x0; time_span > 0; time_span -= h) {
+        for (x = x0; time_span > 0; time_span -= step) {
             dx = f(x);
-            x += h * dx;
+            x += step * dx;
         }
         return x;
     }
 
     // 4th order Runge-Kutta (RK4) method
-    inline ValueType RungeKutta4(Scalar time_span, const ValueType &x0)
+    inline ValueType RungeKutta4(Scalar time_span, Scalar step, const ValueType &x0)
     {
         ValueType x, k1, k2, k3, k4;
-        for (x = x0; time_span > 0; time_span -= h) {
+        for (x = x0; time_span > 0; time_span -= step) {
             k1 = f(x);
-            k2 = f(x + h / 2 * k1);
-            k3 = f(x + h / 2 * k2);
-            k4 = f(x + h  * k3);
-            x += h / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+            k2 = f(x + step / 2 * k1);
+            k3 = f(x + step / 2 * k2);
+            k4 = f(x + step  * k3);
+            x += step / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
         }
         return x;
     }
 
 public:
-    /** Numerical integrator constructor
-     * \param time_step integration step size
-     * \note In case of Runke-Kutta method the intermediate step size is half of
-             time_step size.
-     */
-    NumericalIntegration(Scalar time_step) : h(time_step) {}
-
     /** Integrate given functor
      * \param time_span integration time span
+     * \param step integration time step
      * \param x0 initial value
      * \return integration result
      */
-    ValueType integrate(Scalar time_span, const ValueType &x0)
+    ValueType integrate(Scalar time_span, Scalar step, const ValueType &x0)
     {
         switch (mode) {
             case EULER:
-                return Euler(time_span, x0);
+                return Euler(time_span, step, x0);
 
             case RK4: // fall through
             default:
-                return RungeKutta4(time_span, x0);
+                return RungeKutta4(time_span, step, x0);
         }
     }
 };
--- test/NumericalIntegrationTest.cpp
+++ test/NumericalIntegrationTest.cpp
@@ -12,18 +12,18 @@ struct ZeroDynamics
 };
 
 TEST(EulerTestCase, TestZeroDynamics) {
-    NumericalIntegration<ZeroDynamics, EULER> ni(1e-3);
+    NumericalIntegration<ZeroDynamics, EULER> ni;
     double res;
     const double x0 = 42;
-    res = ni.integrate(1, x0);
+    res = ni.integrate(1, 1e-3, x0);
     ASSERT_EQ(x0, res);
 }
 
 TEST(RungeKuttaTestCase, TestZeroDynamics) {
-    NumericalIntegration<ZeroDynamics, RK4> ni(1e-3);
+    NumericalIntegration<ZeroDynamics, RK4> ni;
     double res;
     const double x0 = 42;
-    res = ni.integrate(1, x0);
+    res = ni.integrate(1, 1e-3, x0);
     ASSERT_EQ(x0, res);
 }
 
@@ -35,18 +35,18 @@ struct ConstDynamics
 };
 
 TEST(EulerTestCase, TestConstDynamics) {
-    NumericalIntegration<ConstDynamics, EULER> ni(1e-3);
+    NumericalIntegration<ConstDynamics, EULER> ni;
     double res;
     const double x0 = 0;
-    res = ni.integrate(2, x0);
+    res = ni.integrate(2, 1e-3, x0);
     EXPECT_NEAR(42.0, res, 0.1);
 }
 
 TEST(RungeKuttaTestCase, TestConstDynamics) {
-    NumericalIntegration<ConstDynamics, RK4> ni(1e-3);
+    NumericalIntegration<ConstDynamics, RK4> ni;
     double res;
     const double x0 = 0;
-    res = ni.integrate(2, x0);
+    res = ni.integrate(2, 1e-3, x0);
     EXPECT_NEAR(42.0, res, 0.1);
 }
 
@@ -62,21 +62,21 @@ struct FreeFall
 };
 
 TEST(EulerTestCase, TestMultivariateIntegration) {
-    NumericalIntegration<FreeFall, EULER> ni(0.01);
+    NumericalIntegration<FreeFall, EULER> ni;
     Vec2d res;
     const Vec2d x0(0, 0);
     double delta_t = 3.0;
-    res = ni.integrate(delta_t, x0);
+    res = ni.integrate(delta_t, 0.01, x0);
     EXPECT_NEAR(-0.5*9.81*delta_t*delta_t, res[0], 1.0);
     EXPECT_NEAR(-9.81*delta_t, res[1], 1.0);
 }
 
 TEST(RungeKuttaTestCase, TestMultivariateIntegration) {
-    NumericalIntegration<FreeFall, RK4> ni(0.1);
+    NumericalIntegration<FreeFall, RK4> ni;
     Vec2d res;
     const Vec2d x0(0, 0);
     double delta_t = 3.0;
-    res = ni.integrate(delta_t, x0);
+    res = ni.integrate(delta_t, 0.1, x0);
     EXPECT_NEAR(-0.5*9.81*delta_t*delta_t, res[0], 1e-6);
     EXPECT_NEAR(-9.81*delta_t, res[1], 1e-6);
 }
