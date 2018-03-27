14b44a96aeec2bc388afabf21da22ca2bdf719f5
Michael Spieler <michael.spieler@epfl.ch>
2018-03-27 15:54:13 +0200

    playground: use EKF module, simpler Dynamics class

--- playground/main.cpp
+++ playground/main.cpp
@@ -5,21 +5,28 @@
 #include <math.h>
 #include "Eigen/Dense"
 #include "../NumericalIntegration.h"
+#include "../ExtendedKalmanFilter.h"
 
 using namespace kalmanfilter;
 
+struct Control {
+    float v;
+    float phi;
+};
+
 struct Dynamics {
 public:
-    static constexpr unsigned nx = 4;
-    static constexpr unsigned nu = 2;
+    // mandatory fields for ExtendedKalmanFilter class
     using Scalar = float;
-    using State = Eigen::Matrix<Scalar, nx, 1>; // x,y,v,theta
-    using Control = Eigen::Matrix<Scalar, nu, 1>;
-    using Jacobian = Eigen::Matrix<Scalar, nx, nx>;
-    using ProcessNoiseCov = Eigen::Matrix<Scalar, nx, nx>;
+    using State = Eigen::Matrix<float, 4, 1>; // x,y,v,theta
+    using Control = struct Control;
+
+    // helper types
+    using Jacobian = Eigen::Matrix<float, 4, 4>;
+    using ProcessNoiseCov = Eigen::Matrix<float, 4, 4>;
 
     ProcessNoiseCov Q;
-    const Scalar B =  0.1; // [m] base distance between front and back wheel
+    const float B =  0.1; // [m] base distance between front and back wheel
 
     Dynamics()
     {
@@ -29,44 +36,40 @@ public:
 
     State operator()(const State &x, const Control &u)
     {
-        const Scalar v = u[0];
-        const Scalar phi = u[1];
-        const Scalar theta = x[3];
+        const float theta = x[3];
         State dx(
-            v * cosf(theta),
-            v * sinf(theta),
+            u.v * cosf(theta),
+            u.v * sinf(theta),
             0,
-            v / B * tanf(phi)
+            u.v / B * tanf(u.phi)
             );
         return dx;
     }
 
     Jacobian jacobian(const State &x, const Control &u)
     {
-        const Scalar v = u[0];
-        const Scalar phi = u[1];
-        const Scalar theta = x[3];
-        const Scalar ct = cosf(theta);
-        const Scalar st = sinf(theta);
+        const float theta = x[3];
+        const float ct = cosf(theta);
+        const float st = sinf(theta);
         Jacobian J;
         J.setZero();
         J.coeffRef(0, 2) = ct;
-        J.coeffRef(0, 3) = -v * st;
+        J.coeffRef(0, 3) = -u.v * st;
         J.coeffRef(1, 2) = st;
-        J.coeffRef(1, 3) = v * ct;
-        J.coeffRef(3, 2) = 1 / B * tanf(phi);
+        J.coeffRef(1, 3) = u.v * ct;
+        J.coeffRef(3, 2) = 1 / B * tanf(u.phi);
         return J;
     }
 };
 
 struct Observation {
-    using Scalar = float;
-    static constexpr unsigned nx = 4;
-    static constexpr unsigned nz = 4;
-    using State = Eigen::Matrix<Scalar, nx, 1>;
-    using Measurement = Eigen::Matrix<Scalar, nz, 1>;
-    using MeasurementNoiseCov = Eigen::Matrix<Scalar, nz, nz>;
-    using Jacobian = Eigen::Matrix<Scalar, nz, nz>;
+    // mandatory fields for ExtendedKalmanFilter class
+    using Measurement = Eigen::Matrix<float, 4, 1>;
+
+    // helper types
+    using State = Eigen::Matrix<float, 4, 1>;
+    using MeasurementNoiseCov = Eigen::Matrix<float, 4, 4>;
+    using Jacobian = Eigen::Matrix<float, 4, 4>;
 
     MeasurementNoiseCov R;
 
@@ -89,12 +92,11 @@ struct Observation {
     }
 };
 
-using State = Dynamics::State;
-using Measurement = Observation::Measurement;
+using EKF = ExtendedKalmanFilter<Dynamics, Observation>;
 
-Measurement from_imu(State x, float acc_x, float acc_y, float gyro_z, float dt)
+EKF::Measurement from_imu(EKF::State x, float acc_x, float acc_y, float gyro_z, float dt)
 {
-    Measurement z;
+    EKF::Measurement z;
     const float v = x[2];
     const float theta = x[3];
     const float ct = cosf(theta);
@@ -106,105 +108,27 @@ Measurement from_imu(State x, float acc_x, float acc_y, float gyro_z, float dt)
     return z + x;
 }
 
-template <typename Dynamics, typename Observation>
-class KalmanFilter {
-public:
-    using Scalar = typename Dynamics::Scalar;
-    using State = typename Dynamics::State;
-    using Control = typename Dynamics::Control;
-    using Measurement = typename Observation::Measurement;
-    using StateCov = Eigen::Matrix<Scalar, Dynamics::nx, Dynamics::nx>;
-    static constexpr unsigned nx = Dynamics::nx;
-    static constexpr unsigned nz = Observation::nz;
-
-    NumericalIntegration<Dynamics> f;
-    Observation h;
-    State x; // state vector
-    StateCov P; // state covariance
-
-    KalmanFilter(State x0, StateCov P0): x(x0), P(P0)
-    {
-
-    }
-
-    void predict(const Control &u, Scalar delta_t)
-    {
-        typename Dynamics::Jacobian A;
-        A.setIdentity();
-
-        A += delta_t*f.jacobian(x, u);
-        x = f.integrate(delta_t, delta_t / 10, x, u);
-        // x = A * x
-        P = A * P * A.transpose() + f.Q;
-    }
-
-    void correct(const Measurement &z)
-    {
-        Measurement y;                      // innovation
-        Eigen::Matrix<Scalar, Observation::nz, Observation::nz> S;    // innovation covariance
-        Eigen::Matrix<Scalar, nx, nz> K;    // Kalman gain
-        typename Observation::Jacobian H;   // jacobian of h
-        Eigen::Matrix<Scalar, nx, nx> IKH;  // temporary matrix
-        Eigen::Matrix<Scalar, nx, nx> I;
-        I.setIdentity();
-
-        H = h.jacobian(x);
-        S = H * P * H.transpose() + h.R;
-        K = P * H.transpose() * S.inverse();
-
-        /* variant 2
-           Eigen::Matrix<Scalar, nz, nz> Sinv;
-           Eigen::Matrix<Scalar, nz, nz> Iz;
-           Iz.setIdentity();
-           Sinv = S.llt().solve(Iz);
-           K = P * H.transpose() * Sinv;
-         */
-
-        /* variant 3
-           Eigen::Matrix<Scalar, nz, nx> A;
-           A = H*P.transpose();
-           // using S = S.transpose()
-           K = S.llt().solve(A).transpose();
-         */
-
-        y = z - h(x);
-        IKH = (I - K * H);
-
-        // measurement update
-        x = x + K * y;
-        P = IKH * P * IKH.transpose() + K * h.R * K.transpose();
-    }
-
-    State update(const Control &u, const Measurement &z, Scalar delta_t)
-    {
-        predict(u, delta_t);
-        correct(z);
-        return x;
-    }
-};
-
 int main(int argc, char *argv[])
 {
 
-    Dynamics::Control u;
-    Measurement z;
-    State x;
-    KalmanFilter<Dynamics, Observation>::StateCov P;
+    EKF::Control u;
+    EKF::Measurement z;
+    EKF::State x;
+    EKF::StateCov P;
 
     x << 0, 0, 0, 0;
     P.setZero();
     P.diagonal() << 0.1f, 0.1f, 0.1f, 0.1f;
 
-    KalmanFilter<Dynamics, Observation> ekf(x, P);
+    EKF ekf(x, P);
 
     const float Ts = 0.02;
     std::ifstream data("imu.txt");
     std::string line;
     while (std::getline(data, line)) {
-        float v, phi;
         float ax, ay, gz;
-        sscanf(&line[0], "%f,%f,%f,%f,%f", &v, &phi, &ax, &ay, &gz);
-        u << v, phi;
+        sscanf(&line[0], "%f,%f,%f,%f,%f", &u.v, &u.phi, &ax, &ay, &gz);
+
         z = from_imu(x, ax, ay, gz, Ts);
 
         x = ekf.update(u, z, Ts);
