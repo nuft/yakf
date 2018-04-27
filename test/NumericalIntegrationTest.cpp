aed23172f9a784ae5ac8a5c297298e5e8b256f81
Michael Spieler <michael.spieler@epfl.ch>
2018-04-27 13:24:24 +0200

    Fix unit tests

--- test/NumericalIntegrationTest.cpp
+++ test/NumericalIntegrationTest.cpp
@@ -4,6 +4,8 @@
 
 using namespace kalmanfilter;
 
+using Vec1d = double;
+
 struct ZeroDynamics
 {
     using State = double;
@@ -16,7 +18,7 @@ TEST(EulerTestCase, TestZeroDynamics) {
     NumericalIntegration<ZeroDynamics, EULER> f;
     Vec1d res;
     const Vec1d x0(42);
-    res = f.integrate(1, 1000, x0, 0.0);
+    res = f.integrate(1, x0, 0.0, 1000);
     ASSERT_EQ(x0, res);
 }
 
@@ -24,7 +26,7 @@ TEST(RungeKuttaTestCase, TestZeroDynamics) {
     NumericalIntegration<ZeroDynamics, RK4> f;
     Vec1d res;
     const Vec1d x0(42);
-    res = f.integrate(1, 1000, x0, 0.0);
+    res = f.integrate(1, x0, 0.0, 1000);
     ASSERT_EQ(x0, res);
 }
 
@@ -40,16 +42,16 @@ TEST(EulerTestCase, TestConstDynamics) {
     NumericalIntegration<ConstDynamics, EULER> f;
     Vec1d res;
     const Vec1d x0(0);
-    res = f.integrate(2, 1000, x0, 0.0);
-    EXPECT_NEAR(42.0, res[0], 0.1);
+    res = f.integrate(2, x0, 0.0, 1000);
+    EXPECT_NEAR(42.0, res, 0.1);
 }
 
 TEST(RungeKuttaTestCase, TestConstDynamics) {
     NumericalIntegration<ConstDynamics, RK4> f;
     Vec1d res;
     const Vec1d x0(0);
-    res = f.integrate(2, 1000, x0, 0.0);
-    EXPECT_NEAR(42.0, res[0], 0.1);
+    res = f.integrate(2, x0, 0.0, 1000);
+    EXPECT_NEAR(42.0, res, 0.1);
 }
 
 using Vec2d = Eigen::Vector2d;
@@ -65,21 +67,21 @@ struct FreeFall
 };
 
 TEST(EulerTestCase, TestMultivariateIntegration) {
-    NumericalIntegration<FreeFall, EULER> ni;
+    NumericalIntegration<FreeFall, EULER> f;
     Vec2d res;
     const Vec2d x0(0, 0);
     double delta_t = 3.0;
-    res = f.integrate(delta_t, 300, x0, 0.0);
+    res = f.integrate(delta_t, x0, 0.0, 300);
     EXPECT_NEAR(-0.5 * 9.81 * delta_t * delta_t, res[0], 1.0);
     EXPECT_NEAR(-9.81 * delta_t, res[1], 1.0);
 }
 
 TEST(RungeKuttaTestCase, TestMultivariateIntegration) {
-    NumericalIntegration<FreeFall, RK4> ni;
+    NumericalIntegration<FreeFall, RK4> f;
     Vec2d res;
     const Vec2d x0(0, 0);
     double delta_t = 3.0;
-    res = f.integrate(delta_t, 300, x0, 0.0);
+    res = f.integrate(delta_t, x0, 0.0, 300);
     EXPECT_NEAR(-0.5 * 9.81 * delta_t * delta_t, res[0], 1e-6);
     EXPECT_NEAR(-9.81 * delta_t, res[1], 1e-6);
 }
